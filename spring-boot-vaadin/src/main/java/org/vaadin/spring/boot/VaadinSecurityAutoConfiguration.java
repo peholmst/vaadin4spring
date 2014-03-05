c2d189a7d240b7e0d9fe53f20face3e24c1ceae0
Petter Holmstroem <petter@vaadin.com>
2014-03-05 09:55:40 +0200

    Removed boot security configuration. It should not be applied automatically.

--- spring-boot-vaadin/src/main/java/org/vaadin/spring/boot/VaadinSecurityAutoConfiguration.java
+++ spring-boot-vaadin/src/main/java/org/vaadin/spring/boot/VaadinSecurityAutoConfiguration.java
@@ -15,27 +15,13 @@
  */
 package org.vaadin.spring.boot;
 
-import com.vaadin.ui.UI;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.vaadin.spring.VaadinUI;
-import org.vaadin.spring.boot.security.ExternalUIAuthentication;
-import org.vaadin.spring.boot.security.SelfContainedAuthentication;
 import org.vaadin.spring.security.EnableVaadinSecurity;
 import org.vaadin.spring.security.Security;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
 
 /**
  * @author Petter Holmström (petter@vaadin.com)
@@ -49,68 +35,10 @@ public class VaadinSecurityAutoConfiguration {
 
     @Configuration
     @EnableVaadinSecurity
-    static class EnableVaadinSecurityConfiguration extends WebSecurityConfigurerAdapter implements InitializingBean {
-
-        @Autowired
-        ApplicationContext applicationContext;
-
-        @Autowired
-        Environment environment;
-
-        private String servletPath;
-
+    static class EnableVaadinSecurityConfiguration implements InitializingBean {
         @Override
         public void afterPropertiesSet() throws Exception {
             logger.debug(getClass().getName() + " has finished running");
-            String servletMapping = environment.getProperty(VaadinAutoConfiguration.EnableVaadinConfiguration.SERVLET_URL_MAPPING_PARAMETER_NAME,
-                    VaadinAutoConfiguration.EnableVaadinConfiguration.DEFAULT_SERVLET_URL_MAPPING);
-
-            if (!servletMapping.endsWith("/*")) {
-                throw new IllegalStateException(String.format("Invalid servlet mapping [%s]. The mapping must end with [/*]", servletMapping));
-            }
-            servletPath = servletMapping.substring(0, servletMapping.length() - 2);
-        }
-
-
-        @Override
-        public void configure(WebSecurity web) throws Exception {
-        }
-
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            logger.debug("Allowing access to Vaadin static resources");
-            http.authorizeRequests().antMatchers("/VAADIN/*").permitAll();
-
-            final Collection<Class<? extends UI>> permittedUIs = new HashSet<>();
-            for (Class<? extends UI> uiClass : getUIClasses()) {
-                if (uiClass.isAnnotationPresent(SelfContainedAuthentication.class)) {
-                    permittedUIs.add(uiClass);
-                } else if (uiClass.isAnnotationPresent(ExternalUIAuthentication.class)) {
-                    permittedUIs.add(uiClass.getAnnotation(ExternalUIAuthentication.class).authenticationUI());
-                    // TODO Forward to authentication UI when authentication is required
-                }
-            }
-            for (Class<? extends UI> permittedUiClass : permittedUIs) {
-                final String urlPattern = getUrlPatternOfUI(permittedUiClass);
-                logger.debug(String.format("Allowing access to Vaadin UI [%s] mapped to URL [%s]", permittedUiClass.getCanonicalName(), urlPattern));
-                http.authorizeRequests().antMatchers(urlPattern).permitAll();
-            }
-        }
-
-        private String getUrlPatternOfUI(Class<? extends UI> uiClass) {
-            return servletPath + uiClass.getAnnotation(VaadinUI.class).path() + "/*";
-        }
-
-        private Collection<Class<? extends UI>> getUIClasses() {
-            final String[] beanNames = applicationContext.getBeanNamesForType(UI.class);
-            final Collection<Class<? extends UI>> beanClasses = new ArrayList<>(beanNames.length);
-            for (String beanName : beanNames) {
-                Class<? extends UI> uiType = (Class<? extends UI>) applicationContext.getType(beanName);
-                if (uiType.isAnnotationPresent(VaadinUI.class)) {
-                    beanClasses.add(uiType);
-                }
-            }
-            return beanClasses;
         }
     }
 }
