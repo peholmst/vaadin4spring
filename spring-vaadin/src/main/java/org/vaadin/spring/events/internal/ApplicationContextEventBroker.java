a472defecc16605365036f983762eba5999c10a8
Petter Holmstroem <petter@vaadin.com>
2014-03-06 11:02:44 +0200

    Added test for ApplicationContextEventBroker

--- spring-vaadin/src/main/java/org/vaadin/spring/events/internal/ApplicationContextEventBroker.java
+++ spring-vaadin/src/main/java/org/vaadin/spring/events/internal/ApplicationContextEventBroker.java
@@ -17,9 +17,12 @@ package org.vaadin.spring.events.internal;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 import org.vaadin.spring.events.EventBus;
+import org.vaadin.spring.events.EventBusScope;
+import org.vaadin.spring.events.EventScope;
 
 /**
  * An {@link org.springframework.context.ApplicationListener} that will forward all received events to an {@link org.vaadin.spring.events.EventBus}.
@@ -32,7 +35,8 @@ public class ApplicationContextEventBroker implements ApplicationListener<Applic
 
     private final EventBus eventBus;
 
-    public ApplicationContextEventBroker(EventBus eventBus) {
+    @Autowired
+    public ApplicationContextEventBroker(@EventBusScope(EventScope.APPLICATION) EventBus eventBus) {
         this.eventBus = eventBus;
     }
 
---
+++ spring-vaadin/src/test/java/org/vaadin/spring/events/internal/ApplicationContextEventBrokerTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2014 The original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.vaadin.spring.events.internal;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationEvent;
+import org.vaadin.spring.events.EventBus;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Test case for {@link org.vaadin.spring.events.internal.ApplicationContextEventBroker}.
+ *
+ * @author Petter Holmström (petter@vaadin.com)
+ */
+public class ApplicationContextEventBrokerTest {
+
+    @Test
+    public void testOnApplicationEvent() {
+        ApplicationEvent event = new ApplicationEvent(this) {
+            @Override
+            public Object getSource() {
+                return "mySource";
+            }
+        };
+        EventBus eventBus = mock(EventBus.class);
+        new ApplicationContextEventBroker(eventBus).onApplicationEvent(event);
+        verify(eventBus).publish("mySource", event);
+    }
+
+}
