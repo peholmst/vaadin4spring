cf4743096c8b0c68b63e8b52ff0dd25304bcf106
Nicolas Frankel <nicolas@frankel.ch>
2014-12-20 20:51:11 +0100

    Initial commit for the explicit wiring MVP module

---
+++ spring-vaadin-simple-mvp/src/test/java/org/vaadin/spring/mvp/autowired/AnnotatedFooView.java
@@ -0,0 +1,9 @@
+package org.vaadin.spring.mvp.autowired;
+
+import org.vaadin.spring.UIScope;
+import org.vaadin.spring.VaadinComponent;
+import org.vaadin.spring.mvp.FooView;
+
+@UIScope
+@VaadinComponent
+public class AnnotatedFooView extends FooView {}
