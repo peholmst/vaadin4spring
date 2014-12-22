73355c51548fceb1861ffbdfc82b16d28fd39553
Gert-Jan Timmer <Gert-Jan.Timmer@enovation.nl>
2014-12-22 11:36:30 +0100

    Added JavaDoc

--- spring-vaadin/src/main/java/org/vaadin/spring/context/VaadinApplicationContext.java
+++ spring-vaadin/src/main/java/org/vaadin/spring/context/VaadinApplicationContext.java
@@ -8,18 +8,37 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.vaadin.spring.events.EventBus;
 
-
+/**
+ * VaadinApplicationContext allows static access to the {@link org.springframework.context.ApplicationContext}.
+ * This implementation exists to provide access from non-managed spring beans.
+ * 
+ * <p>An VaadinApplicationContext provides:
+ * <ul>
+ * <li>Access to the Spring {@link org.springframework.context.ApplicationContext}.
+ * <li>Access to the Vaadin {@link org.vaadin.spring.event.EventBus}
+ * </ul> * 
+ * 
+ * @author G.J.R. Timmer
+ * @see org.springframework.context.ApplicationContext
+ * @see org.vaadin.spring.event.EventBus
+ */
 public class VaadinApplicationContext implements InitializingBean, ApplicationContextAware {
 
 	private static Logger logger = LoggerFactory.getLogger(VaadinApplicationContext.class);
 	
 	private static ApplicationContext context;
 	
+	/**
+	 * @see {@link import org.springframework.context.ApplicationContextAware}
+	 */
 	@Override
 	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
 		context = applicationContext;
 	}
 
+	/**
+	 * @see {@link org.springframework.beans.factory.InitializingBean}
+	 */
 	@Override
 	public void afterPropertiesSet() throws Exception {
 		
@@ -27,10 +46,18 @@ public class VaadinApplicationContext implements InitializingBean, ApplicationCo
 		
 	}
 	
+	/**
+	 * Return the spring {@link org.springframework.context.ApplicationContext}
+	 * @return the spring {@link org.springframework.context.ApplicationContext}
+	 */
 	public static ApplicationContext getContext() {
 		return context;
 	}
 
+	/**
+	 * Return the Vaadin4Spring {@link org.vaadin.spring.events.EventBus}
+	 * @return the Vaadin4Spring {@link org.vaadin.spring.events.EventBus}
+	 */
 	public static EventBus getEventBus() {
 		return context.getBean(EventBus.class);
 	}
