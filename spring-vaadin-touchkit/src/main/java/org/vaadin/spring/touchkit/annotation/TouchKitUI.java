22d0ef2c43cb848545ee71c68b45ab7c2f217bf4
Gert-Jan Timmer <gjr.timmer@gmail.com>
2015-01-16 10:23:27 +0100

    Moved TouchKit Annotation => org.vaadin.spring.touchkit.annotation

--- spring-vaadin-touchkit/src/main/java/org/vaadin/spring/touchkit/annotation/TouchKitUI.java
+++ spring-vaadin-touchkit/src/main/java/org/vaadin/spring/touchkit/annotation/TouchKitUI.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.vaadin.spring.touchkit;
+package org.vaadin.spring.touchkit.annotation;
 
 import org.vaadin.spring.annotation.VaadinComponent;
 import org.vaadin.spring.annotation.VaadinUIScope;
