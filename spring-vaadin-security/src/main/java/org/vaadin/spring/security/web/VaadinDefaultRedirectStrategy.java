a4c07f016e773d1b26eafcd207a08f08131314f9
Gert-Jan Timmer <gjr.timmer@gmail.com>
2015-02-01 16:55:17 +0100

    Added VaadinRedirectStrategy to vaadin4Spring based upon spring-security RedirectStrategy

---
+++ spring-vaadin-security/src/main/java/org/vaadin/spring/security/web/VaadinDefaultRedirectStrategy.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2015 The original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.vaadin.spring.security.web;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.util.UrlUtils;
+import org.vaadin.spring.http.HttpService;
+
+import com.vaadin.ui.UI;
+
+/**
+ * Simple implementation of <tt>RedirectStrategy</tt> which is the default used throughout the vaadin4spring framework.
+ * <br><br>
+ * This class is based upon {@link DefaultRedirectStrategy}
+ * 
+ */
+public class VaadinDefaultRedirectStrategy implements VaadinRedirectStrategy {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+    
+    private boolean contextRelative;
+    
+    @Autowired
+    private HttpService http;
+    
+    @Override
+    public void sendRedirect(String url) {
+        HttpServletRequest request = http.getCurrentRequest();
+        HttpServletResponse response = http.getCurrentResponse();
+        
+        String redirectUrl = calculateRedirectUrl(request.getContextPath(), url);
+        redirectUrl = response.encodeRedirectURL(redirectUrl);
+
+        if (logger.isDebugEnabled()) {
+            logger.debug("Redirecting to '" + redirectUrl + "'");
+        }
+
+        // Change to Vaadin Redirect
+        //response.sendRedirect(redirectUrl);
+        UI.getCurrent().getPage().setLocation(redirectUrl);
+    }
+    
+    private String calculateRedirectUrl(String contextPath, String url) {
+        if (!UrlUtils.isAbsoluteUrl(url)) {
+            if (contextRelative) {
+                return url;
+            } else {
+                return contextPath + url;
+            }
+        }
+
+        // Full URL, including http(s)://
+
+        if (!contextRelative) {
+            return url;
+        }
+
+        // Calculate the relative URL from the fully qualified URL, minus the last
+        // occurrence of the scheme and base context.
+        url = url.substring(url.lastIndexOf("://") + 3); // strip off scheme
+        url = url.substring(url.indexOf(contextPath) + contextPath.length());
+
+        if (url.length() > 1 && url.charAt(0) == '/') {
+            url = url.substring(1);
+        }
+
+        return url;
+    }
+
+    /**
+     * If <tt>true</tt>, causes any redirection URLs to be calculated minus the protocol
+     * and context path (defaults to <tt>false</tt>).
+     */
+    public void setContextRelative(boolean useRelativeContext) {
+        this.contextRelative = useRelativeContext;
+    }
+}
