e5d22bfe08f88adf37882ad44cf0ce69393a37e8
Petter Holmström <petter@vaadin.com>
2016-01-22 14:52:12 +0200

    Added auto-configuration of Atmosphere SessionSupport. Don't know if it is actually needed, but #169 hints this might be the case. Closes #169.

---
+++ extensions/boot/src/main/java/org/vaadin/spring/boot/AtmosphereAutoConfiguration.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2016 The original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.vaadin.spring.boot;
+
+import org.atmosphere.cpr.SessionSupport;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.context.embedded.ServletListenerRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Auto configuration for Atmosphere (used by Vaadin websocket push)
+ * 
+ * @author Petter Holmström (petter@vaadin.com)
+ */
+@Configuration
+@ConditionalOnClass(SessionSupport.class)
+public class AtmosphereAutoConfiguration {
+
+    private static Logger logger = LoggerFactory.getLogger(AtmosphereAutoConfiguration.class);
+
+    @Configuration
+    static class AtmosphereSessionSupportConfiguration implements InitializingBean {
+
+        @Override
+        public void afterPropertiesSet() throws Exception {
+            logger.debug("{} initialized", getClass().getName());
+        }
+
+        @Bean
+        ServletListenerRegistrationBean<SessionSupport> atmosphereSessionSupport() {
+            ServletListenerRegistrationBean<SessionSupport> bean = new ServletListenerRegistrationBean<SessionSupport>(
+                new SessionSupport());
+            return bean;
+        }
+    }
+}
